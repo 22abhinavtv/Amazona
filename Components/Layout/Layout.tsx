10ec0b7c9cd109add12eff3b76674f6517c86376
Abhinav TV <cworled6028@gmail.com>
2022-09-19 16:02:54 +0530

    Fooeter just started

--- Components/Layout/Layout.tsx
+++ Components/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { Grid } from '@mui/material'
 import React from 'react'
+import Footer from '../UI/Footer'
 import NavBar from '../UI/NavBar'
 
 export default function Layout() {
   return (
     <Grid>
         <NavBar/>
+        <Footer/>
     </Grid>
   )
 }
---
+++ Components/UI/Footer.tsx
@@ -0,0 +1,8 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <Grid>Footer</Grid>
+  )
+}
